5f0d6502b905197616d2aaaca1dcd907179dcd04
alphang <alphang@server.fake>
2013-03-20 23:03:11 -0700

    Add Admin::StatesController for managing states

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :find_states
 
   private
 
   def authorize_admin!
-      authenticate_user! # Method provided by Devise
-      unless current_user.admin?
-        flash[:alert] = "You must be an admin to do that."
-        redirect_to root_path
-      end
+    authenticate_user! # Method provided by Devise
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
     end
+  end
+
+  def find_states
+    @states = State.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Ticketee::Application.routes.draw do
     resources :users do
       resources :permissions
     end
+    resources :states
   end
 
   root :to => "projects#index" #Project controller's index action
